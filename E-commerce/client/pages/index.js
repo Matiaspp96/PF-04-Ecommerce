e807a4d11656017dcaf76c382167bd060a1601b0
Virginia <virginia.carmuega@gmail.com>
2022-05-18 08:31:41 -0300

    feat(Component/Card): Basic structure and styles of the Card and Cards components to show list of products made

--- E-commerce/client/pages/index.js
+++ E-commerce/client/pages/index.js
@@ -1,10 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Cards from '../components/Card/Cards.js'
+import axios from 'axios'
 
 
-export default function Home() {
+export default function Home({msg}) {
   return (
-    <div>
+    <div> 
+      <Cards productos={msg}/>
     </div>
   )
+}
+
+Home.getInitialProps = async (ctx) => {
+  const res = await axios.get('https://fakestoreapi.com/products')
+  const producto = res.data;
+  return {
+    msg: producto
+  }
 }
