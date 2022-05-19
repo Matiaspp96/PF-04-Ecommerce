f1788b85f8cfc2333d534b2cae9d7fa875ac7e3c
Virginia <virginia.carmuega@gmail.com>
2022-05-19 08:30:22 -0300

    feat(componente/Card): Link to detail page added

--- E-commerce/client/components/Card/Card.js
+++ E-commerce/client/components/Card/Card.js
@@ -1,8 +1,9 @@
 import { Text, Flex, Box, GridItem, Image } from '@chakra-ui/react'
+import Link from 'next/link';
 
 export default function Card({ producto }) {
   
-  const { title, image, category, price } = producto;
+  const { title, image, category, price, id } = producto;
   
   return (
       <GridItem h='250' overflow='auto'>
@@ -13,6 +14,12 @@ export default function Card({ producto }) {
               <Text fontSize='xl'>{title.substring(0,15)}</Text>
               <Text as='i'>{category}</Text>
               <Text fontWeight='bold'>${price}</Text>
+              <Link href={{
+                pathname: 'product/[id]',
+                query: {product: 'product', id:id}
+              }}>
+                <a>Ver Detalle</a>
+              </Link>
       </GridItem>
               
          
