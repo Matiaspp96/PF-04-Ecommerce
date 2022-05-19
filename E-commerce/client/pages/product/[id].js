c6debb6404c81c56e8bd9de00d51aea16593f7da
Virginia <virginia.carmuega@gmail.com>
2022-05-19 08:32:52 -0300

    feat(Page/Producto/:id): Route to detail page of products created

---
+++ E-commerce/client/pages/product/[id].js
@@ -0,0 +1,13 @@
+import Navbar from '../../components/Navbar/Navbar'
+import Detail from '../../components/Detail/Detail'
+
+
+export default function DetailPage() {
+    
+    return (
+      <div>
+          <Navbar />
+          <Detail />
+      </div>
+    )
+  }
