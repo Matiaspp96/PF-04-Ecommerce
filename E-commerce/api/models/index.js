8a99286385b3fdf77b654c9c6030f976d615555e
Matias Palomo <89273108+Matiaspp96@users.noreply.github.com>
2022-05-18 23:19:25 -0300

    Revert "Matias/component/navbar"

--- E-commerce/api/models/index.js
+++
@@ -1,8 +0,0 @@
-const models = {
-  catergoryModel: require('./categories'),
-  orderModel: require(`./orders`),
-  productModel: require(`./products`),
-  userModel: require(`./users`),
-};
-
-module.exports = models;
--- E-commerce/api/utils/handleError.js
+++
@@ -1,6 +0,0 @@
-const handleHttpError = (res, message = "something happen", code = 403) => {
-    res.status(code);
-    res.send({ error: message });
-  };
-  
-  module.exports = { handleHttpError };
