12b168b0028fbe2ae8c3ca3ba468bb135f1e05ba
Virginia <virginia.carmuega@gmail.com>
2022-05-19 08:34:01 -0300

    feat(Component/Detail):Basic structure and styles of component Detail showing +info of a specific product

---
+++ E-commerce/client/components/Detail/Detail.js
@@ -0,0 +1,121 @@
+import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux';
+import Link from 'next/link';
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    VisuallyHidden,
+    List,
+    ListItem,
+  } from '@chakra-ui/react';
+//   import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+//   import { MdLocalShipping } from 'react-icons/md';
+  
+  export default function Detail() {
+    const router = useRouter()
+    const { id } = router.query
+
+    const productos = useSelector((state)=> state.productReducer.products);
+    const product = productos.find((ps)=>{return ps.id === Number(id)});
+    
+    return (
+      <Container maxW={'7xl'}>
+        <Link href={'/'}>
+          <a>Back</a>
+        </Link>
+          {console.log(product)}
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 10, md: 10 }}
+          >
+          <Flex>
+            
+            <Image
+              rounded={'md'}
+              alt={product.title}
+              src={product.image}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={{ base: '100%', sm: '400px', lg: '500px' }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={'header'}>
+              <Text>{product.category}</Text>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                {product.title}
+              </Heading>
+              <Text
+                color={useColorModeValue('gray.900', 'gray.400')}
+                fontWeight={300}
+                fontSize={'2xl'}>
+                {product.price}
+              </Text>
+            </Box>
+  
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.200', 'gray.600')}
+                />
+              }>
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text
+                  color={useColorModeValue('gray.500', 'gray.400')}
+                  fontSize={'2xl'}
+                  fontWeight={'300'}>
+                  {product.description}
+                </Text>
+                <Text
+                color={useColorModeValue('gray.900', 'gray.400')}
+                fontWeight={300}
+                fontSize={'2xl'}>
+                {product.rating.rate}
+              </Text>
+                
+              </VStack>
+            </Stack>
+  
+            <Button
+              rounded={'none'}
+              w={'full'}
+              mt={8}
+              size={'lg'}
+              py={'7'}
+              bg={useColorModeValue('gray.900', 'gray.50')}
+              color={useColorModeValue('white', 'gray.900')}
+              textTransform={'uppercase'}
+              _hover={{
+                transform: 'translateY(2px)',
+                boxShadow: 'lg',
+              }}>
+              Add to cart
+            </Button>
+  
+            {/* <Stack direction="row" alignItems="center" justifyContent={'center'}>
+              <MdLocalShipping />
+              <Text>2-3 business days delivery</Text>
+            </Stack> */}
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    );
+  }
