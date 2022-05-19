0d0728dbceadda81599e19d1341f9fe721d4c205
Virginia <virginia.carmuega@gmail.com>
2022-05-19 19:26:44 -0300

    feat(component/Detail):Improved styles an useEffect added

--- E-commerce/client/components/Detail/Detail.js
+++ E-commerce/client/components/Detail/Detail.js
@@ -1,117 +1,92 @@
 import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux';
-import {
-    Box,
-    chakra,
-    Container,
-    Stack,
-    Text,
-    Image,
-    Flex,
-    VStack,
-    Button,
-    Heading,
-    SimpleGrid,
-    StackDivider,
-    useColorModeValue,
-    VisuallyHidden,
-    List,
-    ListItem,
-  } from '@chakra-ui/react';
-//   import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
-//   import { MdLocalShipping } from 'react-icons/md';
+import { useSelector, useDispatch} from 'react-redux';
+import { useEffect } from 'react'
+import { getDetail } from '../../redux/actions/products.js'
+import { Box, Container, Stack, Text, Image, VStack, Button, Heading, SimpleGrid, useColorModeValue, Center } from '@chakra-ui/react';
   
   export default function Detail() {
     const router = useRouter()
     const { id } = router.query
+    const dispatch = useDispatch();
 
-    const productos = useSelector((state)=> state.productReducer.products);
-    const product = productos.find((ps)=>{return ps.id === Number(id)});
+    useEffect(() => {
+      dispatch(getDetail(id))
+    },[dispatch,id]);
+
+    const product = useSelector((state)=> state.productReducer.details);
     
     return (
       <Container maxW={'7xl'}>
-          {console.log(product)}
+        {product.title ? 
         <SimpleGrid
-          columns={{ base: 1, lg: 2 }}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 10, md: 10 }}
-          >
-          <Flex>
-            
-            <Image
-              rounded={'md'}
-              alt={product.title}
-              src={product.image}
-              fit={'cover'}
-              align={'center'}
-              w={'100%'}
-              h={{ base: '100%', sm: '400px', lg: '500px' }}
-            />
-          </Flex>
-          <Stack spacing={{ base: 6, md: 10 }}>
-            <Box as={'header'}>
-              <Text>{product.category}</Text>
-              <Heading
-                lineHeight={1.1}
-                fontWeight={600}
-                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
-                {product.title}
-              </Heading>
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 10, md: 10 }}>
+        
+          <Image
+            rounded={'md'}
+            alt={product.title}
+            src={product.image}
+            fit={'cover'}
+            align={'center'}
+            w={'100%'}
+            h={{ base: '100%', sm: '400px', lg: '500px' }}
+          />
+        
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box>
+            <Text>{product.category}</Text>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+              {product.title}
+            </Heading>
+            <Text
+              fontWeight={300}
+              fontSize={'2xl'}>
+              {product.price}
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={'column'}>
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                color={useColorModeValue('gray.500', 'gray.400')}
+                fontSize={'2xl'}
+                fontWeight={'300'}>
+                {product.description}
+              </Text>
               <Text
                 color={useColorModeValue('gray.900', 'gray.400')}
                 fontWeight={300}
                 fontSize={'2xl'}>
-                {product.price}
-              </Text>
-            </Box>
-  
-            <Stack
-              spacing={{ base: 4, sm: 6 }}
-              direction={'column'}
-              divider={
-                <StackDivider
-                  borderColor={useColorModeValue('gray.200', 'gray.600')}
-                />
-              }>
-              <VStack spacing={{ base: 4, sm: 6 }}>
-                <Text
-                  color={useColorModeValue('gray.500', 'gray.400')}
-                  fontSize={'2xl'}
-                  fontWeight={'300'}>
-                  {product.description}
-                </Text>
-                <Text
-                color={useColorModeValue('gray.900', 'gray.400')}
-                fontWeight={300}
-                fontSize={'2xl'}>
                 {product.rating.rate}
-              </Text>
-                
-              </VStack>
-            </Stack>
-  
-            <Button
-              rounded={'none'}
-              w={'full'}
-              mt={8}
-              size={'lg'}
-              py={'7'}
-              bg={useColorModeValue('gray.900', 'gray.50')}
-              color={useColorModeValue('white', 'gray.900')}
-              textTransform={'uppercase'}
-              _hover={{
-                transform: 'translateY(2px)',
-                boxShadow: 'lg',
-              }}>
-              Add to cart
-            </Button>
-  
-            {/* <Stack direction="row" alignItems="center" justifyContent={'center'}>
-              <MdLocalShipping />
-              <Text>2-3 business days delivery</Text>
-            </Stack> */}
+              </Text>    
+            </VStack>
           </Stack>
-        </SimpleGrid>
+
+          <Button
+            rounded={'none'}
+            zIndex={'-1'}
+            w={'full'}
+            mt={8}
+            size={'lg'}
+            py={'7'}
+            bg={useColorModeValue('gray.900', 'gray.50')}
+            color={useColorModeValue('white', 'gray.900')}
+            textTransform={'uppercase'}
+            _hover={{
+              transform: 'translateY(2px)',
+              boxShadow: 'lg',
+            }}>
+            Add to cart
+          </Button>
+        </Stack>
+      </SimpleGrid>:
+      <Center><Text>Loading...</Text></Center>}
       </Container>
     );
   }
