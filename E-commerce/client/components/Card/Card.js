9aeec6d26b4af6b2715df0048cefe085d8b6b09d
Matiaspp96 <matiaas.p@gmail.com>
2022-05-19 17:54:01 -0300

    feat:(routes and button: cart, fav)

--- E-commerce/client/components/Card/Card.js
+++ E-commerce/client/components/Card/Card.js
@@ -1,9 +1,37 @@
-import { Text, Flex, Box, GridItem, Image } from '@chakra-ui/react'
+import { Text, GridItem, Image, Button } from '@chakra-ui/react'
+import {IoCartOutline, IoHeartOutline} from 'react-icons/io5'
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from '../../redux/actions/cart';
+import { addItemToFav } from '../../redux/actions/favorites';
+import { handleAddToCartOrFav } from '../../utils/handles';
 
 export default function Card({ producto }) {
-  
-  const { title, image, category, price } = producto;
-  
+  const dispatch = useDispatch()
+  const { title, price, category, image } = producto;
+
+  const itemsCart = useSelector((state) => state.shoppingCartReducer.itemsCart)
+  // const handleAddToCart = (item) => {
+  //   item.preventDefault();
+  //   const product = {
+  //     id,
+  //     title,
+  //     price,
+  //     description ,
+  //     category,
+  //     image
+  //   };
+  //   dispatch(addItemToCart(product))
+  // }
+  function handleAddCartOnClick(e, producto){
+    dispatch(addItemToCart(handleAddToCartOrFav(e, producto)))
+    console.log(itemsCart)
+  }
+
+  function handleAddFavOnClick(e, producto){
+    dispatch(addItemToFav(handleAddToCartOrFav(e, producto)))
+    console.log(producto)
+  }
+
   return (
       <GridItem h='250' overflow='auto'>
               <Image 
@@ -13,10 +41,8 @@ export default function Card({ producto }) {
               <Text fontSize='xl'>{title.substring(0,15)}</Text>
               <Text as='i'>{category}</Text>
               <Text fontWeight='bold'>${price}</Text>
+              <Button onClick={e=>handleAddCartOnClick(e,producto)} color='blackAlpha.800' borderRadius='15px' p='0'><IoCartOutline size='2em'/></Button>
+              <Button onClick={e=>handleAddFavOnClick(e,producto)} color='blackAlpha.800' borderRadius='15px' p='0'><IoHeartOutline size='2em'/></Button>
       </GridItem>
-              
-         
-          
-          
     )
   }
--- E-commerce/client/redux/reducer/shoppingCart.js
+++ E-commerce/client/redux/reducer/shoppingCart.js
@@ -1,15 +1,43 @@
 import {
-  DELETE_PRODUCT ,
+  ADD_ITEM,
+  DELETE_ITEM,
+  GET_ALL_CART,
 } from '../actions/actionstype.js' 
 
 const initialState = {
   backUp: [],
+  itemsCart: [],
+  totalPrice: 0,
 }
 
 export default function shoppingCartReducer(state = initialState, action) {
+  let itemsCart = state.itemsCart
+  let totalPrice = state.totalPrice
   switch(action.type) {
-    case DELETE_PRODUCT :
+    case ADD_ITEM :
+      return{
+        ...state,
+        itemsCart: [...state.itemsCart, action.payload],
+        totalPrice: itemsCart?.reduce((acc,item) => acc + item.price, 0)
+      } 
+    case DELETE_ITEM:
+      const index = itemsCart.findIndex(
+        (cartItem) => cartItem.id === action.payload.id
+      );
+      let newCart = [...itemsCart];
+      if(index >= 0){
+        newCart.splice(index,1)
+      } else{
+        console.warn('Cant remove product')
+      }
       return {
+        ...state,
+        itemsCart: newCart
+      }
+    case GET_ALL_CART:
+      return {
+        ...state,
+        itemsCart
       }
     default:
       return state;
