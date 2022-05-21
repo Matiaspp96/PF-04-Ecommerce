ceadb85127bc08f1d7ef9b8ecb5d12dd2ae586c4
Virginia <virginia.carmuega@gmail.com>
2022-05-21 08:12:53 -0300

    feat(component/card): Improved styles

--- E-commerce/client/components/Card/Card.js
+++ E-commerce/client/components/Card/Card.js
@@ -39,69 +39,68 @@ export default function Card({ producto }) {
 
   return (
       <Stack
-        // width={{base:'40vw', md: '20vw'}}
         h={{base:'240', md:'260', lg:'310'}} 
         overflow='auto'
         boxShadow='lg'
         >
-      <Tag
-            borderRadius={'none'}
-            alignSelf={'flex-start'}
-            fontSize={'x-small'}
-            fontStyle={'italic'}
-          >{category}</Tag>
-          <Center>
+        <Tag
+          borderRadius={'none'}
+          alignSelf={'flex-start'}
+          fontSize={'x-small'}
+          fontStyle={'italic'}>
+            {category}
+        </Tag>
+        <Center>
           <Image 
-                src={image} 
-                alt={title}  
-                boxSize={{base:'80px', md:'100px', lg:'150px'}}
-                alignItems='center'/>
-          </Center>
-          
-              
-          <Stack pl={'1rem'} pe={'1rem'}>
-          
-              
-              <Link href={{
-                pathname: 'product/[id]',
-                query: {id:id}
+            src={image} 
+            alt={title}  
+            boxSize={{base:'80px', md:'100px', lg:'150px'}}
+            alignItems='center'/>
+        </Center>
+
+        <Stack pl={'1rem'} pe={'1rem'}>
+          <Link 
+            href={{
+              pathname: 'product/[id]',
+              query: {id:id}
               }}>
-              <a><Text fontSize='xl' fontWeight='bold'>{title.substring(0,10)}</Text></a>
-              </Link>
+              <a>
+                <Text 
+                  fontSize='xl' 
+                  fontWeight='bold'>
+                    {title.substring(0,10)}
+                </Text>
+              </a>
+          </Link>
               
-              <Flex justifyContent={'space-between'}>
-              <Text color={'#1884BE'}>${price}</Text>
-              <Flex alignItems={'center'}>
+          <Flex justifyContent={'space-between'}>
+            <Text color={'#1884BE'}>${price}</Text>
+            <Flex alignItems={'center'}>
               <Text
                 fontWeight={300}
                 fontSize={'smaller'}
-                me={'.4em'}
-                >
-                {rating.rate}
+                me={'.4em'}>
+                  {rating.rate}
               </Text>
               <IoStarSharp size='1em' />
-              </Flex>
-              </Flex>
+            </Flex>
+          </Flex>
               
-
-              
-              <Flex justifyContent={'space-evenly'} width='100%'>
-                <IconButton 
-                  onClick={e=>handleAddCartOnClick(e,producto)}
-                  backgroundColor='transparent'
-                  icon={<IoCartOutline size='2em'/>}
-                  color={addCart ? '#1884BE' : 'grey'}
-                />
-                <IconButton 
-                  onClick={e=>handleAddFavOnClick(e,producto)}
-                  backgroundColor='transparent'
-                  icon={<IoHeartOutline size='2em'/>}
-                  color={addFavorite ? '#1884BE' : 'grey'}
-                />
-              </Flex>
-          </Stack> 
-          
-              
+          <Flex justifyContent={'space-evenly'} width='100%'>
+            <IconButton 
+              onClick={e=>handleAddCartOnClick(e,producto)}
+              backgroundColor='transparent'
+              icon={<IoCartOutline size='2em'/>}
+              color={addCart ? '#1884BE' : 'grey'}
+            />
+            <IconButton 
+              onClick={e=>handleAddFavOnClick(e,producto)}
+              backgroundColor='transparent'
+              icon={<IoHeartOutline size='2em'/>}
+              color={addFavorite ? '#1884BE' : 'grey'}
+            />
+          </Flex>
+        </Stack>      
       </Stack>
     )
   }
--- E-commerce/client/components/Card/Cards.js
+++ E-commerce/client/components/Card/Cards.js
@@ -10,13 +10,12 @@ export default function Cards() {
     <Box margin={{base: '.5em', md:'1em', lg:'3em'}}>
       <Box>
         <SimpleGrid
-        columns={{ base: 2, sm: 3, md: 4, lg:5 }}
-        
-         gap={'5'} 
-         marginTop='2rem'>
-          {productos.map(ps=>{ return (
-            <Card key={ps.id} producto={ps}></Card>
-          )})}
+          columns={{ base: 2, sm: 3, md: 4, lg:5 }}
+          gap={'5'} 
+          marginTop='2rem'>
+            {productos.map(ps=>{ return (
+              <Card key={ps.id} producto={ps}></Card>
+            )})}
         </SimpleGrid>
       </Box>
     </Box>
