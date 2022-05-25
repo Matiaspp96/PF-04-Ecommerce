034a996c51598a0c476741a2cb94ece4e7065d0d
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-05-25 17:25:20 -0300

    feat(filter by categories) fix redux(actions - reducer)

--- E-commerce/client/redux/reducer/shoppingCart.js
+++ E-commerce/client/redux/reducer/shoppingCart.js
@@ -2,9 +2,10 @@ import Swal from 'sweetalert2'
 import {
   ADD_ITEM,
   DELETE_ITEM,
+  REMOVE_ITEM,
   GET_ALL_CART,
   GET_TOTAL_PRICE,
-  GET_TOTAL_ITEMS
+  GET_TOTAL_ITEMS,
 } from '../actions/actionstype.js' 
 
 const initialState = {
@@ -19,18 +20,19 @@ export default function shoppingCartReducer(state = initialState, action) {
   // let totalPrice = state.totalPrice
   let cart = state.itemsCart
   let totalItems = state.totalItems
+  // console.log(itemsCart)
   switch(action.type) {
     case ADD_ITEM :
-      let itemCart = state.itemsCart?.find(e => e.product.id === action.payload.id)
+      let itemCart = itemsCart?.find(e => e.product._id === action.payload._id)
       if(itemCart){
-        cart = state.itemsCart.map(e => e.product.id === action.payload.id ? {...e, quantity: e.quantity + 1, totalPrice: action.payload.price + e.quantity * e.product.price} : e )
+        cart = state.itemsCart.map(e => e.product._id === action.payload._id ? {...e, quantity: e.quantity + 1, totalPrice: action.payload.price + e.quantity * e.product.price} : e )
       } else {
-        cart = [...state.itemsCart, {product: action.payload, quantity: 1, totalPrice: action.payload.price}] }
+        cart = [...itemsCart, {product: action.payload, quantity: 1, totalPrice: action.payload.price}] }
         return{
           ...state,
           itemsCart: cart,
         } 
-    case DELETE_ITEM:
+    case DELETE_ITEM: //Borrar articulo
       // const index = itemsCart.findIndex(
       //   (cartItem) => cartItem.id === action.payload.id
       // );
@@ -40,7 +42,7 @@ export default function shoppingCartReducer(state = initialState, action) {
       // } else{
       //   console.warn('Cant remove product')
       // }
-      const index = state.itemsCart.findIndex(e => e.product.id === action.payload.id)
+      let index = itemsCart.findIndex(e => e.product.id === action.payload.id)
       // if(itemCart && itemCart.quantity >= 2){
       //   cart = state.itemsCart.map(e => e.product.id === action.payload.id ? {...e, quantity: e.quantity - 1, totalPrice: e.quantity * e.product.price} : e )
       if(index >= 0) {
@@ -51,19 +53,27 @@ export default function shoppingCartReducer(state = initialState, action) {
         ...state,
         itemsCart: cart 
       }
+    case REMOVE_ITEM: //Remover cantidades del articulo
+      index = itemsCart.findIndex(e => e.product._id === action.payload.id)
+      if(index >= 0 && itemsCart[index].quantity > 1){
+        console.log(itemsCart[index].product._id + action.payload.id)
+        cart = itemsCart.map(e => e.product._id === action.payload.id ? {...e, quantity: e.quantity - 1, totalPrice: action.payload.price + (e.quantity - 1) * e.product.price} : e)
+      }
+      return{
+        ...state,
+        itemsCart: cart
+      }
     case GET_ALL_CART:
       return {
         ...state,
         itemsCart
       }
     case GET_TOTAL_PRICE:
-      console.log(itemsCart[0].totalPrice)
       return {
         ...state,
         totalPrice: itemsCart?.reduce((acc,item) => acc + item.totalPrice, 0).toFixed(2) 
       }
     case GET_TOTAL_ITEMS:
-      console.log(totalItems)
       return {
         ...state,
         totalItems: itemsCart?.reduce((acc,item) => acc + item.quantity, 0)
