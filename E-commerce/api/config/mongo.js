a3e3c9a23ee928f4391a9a90c160e577203acba9
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-05-25 17:28:31 -0300

    fix api from config/mongo

--- E-commerce/api/config/mongo.js
+++ E-commerce/api/config/mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const password = 'wercox-qEqqy8-gamdis'
-const uri = `mongodb+srv://admin:${password}@cluster0.0ykiz.mongodb.net/DataBaseEcommerce`;1
+const uri = `mongodb+srv://admin:${password}@cluster0.0ykiz.mongodb.net/DatabaseEcommerce`;1
 
 const dbConnect = () => {
     mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(uri)
   .then(() => console.log(process.env.MONGO_URL))
   .catch((err) => {
     console.log(err);
