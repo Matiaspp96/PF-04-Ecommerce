d213053ef0cf4a972346d348abe978fcaf1e0982
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-05-27 18:04:33 -0300

    feat(actions categories(delete, update, create))

--- E-commerce/client/redux/actions/categories.js
+++ E-commerce/client/redux/actions/categories.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-import { GET_CATEGORIES,
-    FILTER_BY_CATEGORIES,
+import { 
+  GET_CATEGORIES,
+  FILTER_BY_CATEGORIES,
+  CREATE_CATEGORY,
+  DELETE_CATEGORIES,
+  UPDATE_CATEGORY,
 } from './actionstype.js';
 const BASEURL = 'http://localhost:3000/api';
 
@@ -17,20 +21,49 @@ export const getAllCategories = () => async (dispatch) => {
     }
   };
 
-  export function deleteCategory(id) {
-    try {
-      const response = await axios.get(`${BASEURL}/categories/${id}`)
-      return dispatch({
-        type: DELETE_CATEGORIES,
-        payload: response.data
-      })
-    } catch (err) {
-      console.log(error)
-    }
+export async function createCategory(name) {
 
+  // let value = { payload: name }
+  let value = name 
+  // return async function (dispatch) {
+  try {
+    const response = await axios.post(`${BASEURL}/categories`,value);
+    return dispatch({
+        type: CREATE_CATEGORY,
+        payload: response.data
+    })
+  } catch (error) {
+      console.log(error)
+  }
 }
 
-export function filterByCategory(payload) {
+export async function deleteCategory(id){
+  try {
+    const response = await axios.delete(`${BASEURL}/categories/${id}`); 
+    return dispatch({
+      type: DELETE_CATEGORIES,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+export async function updateCategory(name) {
+  let value = name
+  try {
+    const response = await axios.put(`${BASEURL}/categories/${id}`, value);
+    return dispatch({
+      type: UPDATE_CATEGORY,
+      payload: response.data,
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function filterByCategory(payload) {
     return {
        type: FILTER_BY_CATEGORIES,
        payload
