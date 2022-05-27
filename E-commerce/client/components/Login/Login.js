6de541679d921f0109ab1f5f5633df47e0cad501
Virginia <virginia.carmuega@gmail.com>
2022-05-27 17:04:48 -0300

    prueba de rutas de autenticacion

--- E-commerce/client/components/Login/Login.js
+++ E-commerce/client/components/Login/Login.js
@@ -2,7 +2,8 @@ import { Button, Center, FormControl, FormLabel, Input, Stack } from '@chakra-ui
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import axios from 'axios'
-const URL = 'https://3000/api/register'
+const URL = 'https://localhost:3000/api/auth/login/local/signin'
+const Url2= 'http://localhost:3000/api/auth/login/local/signup'
 
 
 const Login = () => {
@@ -11,7 +12,7 @@ const [result, setResult] = useState('')
     username: '',
     password: ''
 })
-const router = useRouter
+const router = useRouter()
 
     function handleSubmit(e){
         e.preventDefault();
@@ -29,20 +30,37 @@ const router = useRouter
       console.log(result)
         
     }
+    function handleSubmitLogin(e){
+        e.preventDefault();
+        axios.post(URL, user)
+        .then(function(response){
+            setResult(response.data) 
+        })
+        .catch(function(error){
+            setResult(error)
+      })
+      setUser({
+        username: '',
+        password: ''
+    })
+      console.log(result)
+        
+    }
 
     function handleGoogleLog(){
-        router.push('')
+        router.push('https://localhost:3000/api/auth/login/google')
     }
 
     function handleChange(e){
         setUser({...user, [e.target.name]: e.target.value})
+        console.log(e.target.value)
     }
 
   return (
     
         <Center>
             <Stack>
-            <FormControl onSubmit={e=>{handleSubmit(e)}}>
+            <FormControl >
                 <Stack>
                     <FormLabel htmlFor="username">Username</FormLabel>
                     <Input id="username"  onChange={handleChange} name="username" type="text" autoComplete="username" required autoFocus />
@@ -51,8 +69,8 @@ const router = useRouter
                     <FormLabel htmlFor="current-password">Password</FormLabel>
                     <Input onChange={handleChange} id="current-password" name="password" type="password" autoComplete="current-password" required />
                 </Stack>
-                <Button type="submit">Sign in</Button>
-                {/* <Button type="submit">Log in</Button> */}
+                <Button onClick={e=>{handleSubmit(e)}}>Sign in</Button>
+                <Button onClick={e=>{handleSubmitLogin(e)}}>Log in</Button>
             </FormControl>
             <Button onClick={handleGoogleLog}>Log in with google</Button>
         
