f505adb74ba3014d717f020a009c903cd8e0f00e
Matiaspp96 <matiaas.p@gmail.com>
2022-05-30 13:03:15 -0300

    feat:(message in '/')

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.status(200).send('<h2>Deploy listo</h2>')
+})
+
 app.use("/api", require("./routes"));
 
 app.listen(port, () =>
