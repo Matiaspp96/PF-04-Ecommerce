c1da85e33ef039b2aafeefa58cec57525734a465
Juan Sebastián Contini <continisebastian@gmail.com>
2022-06-01 20:09:11 -0300

    ultimo cambio

--- E-commerce/api/config/mongo.js
+++ E-commerce/api/config/mongo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const dbConnect = () => {
@@ -9,19 +8,5 @@ const dbConnect = () => {
       console.log(err);
     });
 };
-=======
-const mongoose = require('mongoose')
-const password = 'wercox-qEqqy8-gamdis'
-const uri = `mongodb+srv://admin:${password}@cluster0.0ykiz.mongodb.net/DatabaseEcommerce`;1
-
-const dbConnect = () => {
-    mongoose
-  .connect(uri)
-  .then(() => console.log(process.env.MONGO_URL))
-  .catch((err) => {
-    console.log(err);
-  });
-}
->>>>>>> 11c60b279f905895892de2e2861c4c2b4ffab2c9
 
 module.exports = dbConnect;
