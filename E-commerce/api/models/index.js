26fa73a3d41585f3918f6de3890f2fd68ad6d249
Matiaspp96 <matiaas.p@gmail.com>
2022-06-01 17:46:53 -0300

    fix:(delete /client)

--- E-commerce/api/models/index.js
+++
@@ -1,9 +0,0 @@
-const models = {
-  catergoryModel: require("./categories"),
-  orderModel: require(`./orders`),
-  productModel: require(`./products`),
-  userModel: require(`./users`),
-  reviewModel: require("./reviews"),
-};
-
-module.exports = models;
