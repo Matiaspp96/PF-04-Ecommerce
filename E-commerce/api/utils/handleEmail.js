8c28884b212cbf17f65e872cc0c697e2f8408484
Virginia <virginia.carmuega@gmail.com>
2022-06-09 20:17:41 -0300

    feature(component/login): improved styles and functionalities:login local, reset password

--- E-commerce/api/utils/handleEmail.js
+++ E-commerce/api/utils/handleEmail.js
@@ -8,7 +8,7 @@ const nodemailer = require ("nodemailer");
       secure: true, // true for 465, false para otros puertos
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
       tls: {
         rejectUnauthorized: false
