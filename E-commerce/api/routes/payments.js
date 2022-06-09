4f81e4a778ce02d726d2bd5777b79bb0b8df6d98
arkangel3d <arkangelcontreras@icloud.com>
2022-06-09 15:29:49 -0500

    mercadoPago rutas

--- E-commerce/api/routes/payments.js
+++ E-commerce/api/routes/payments.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
+const {initPaymentMp} = require('../controllers/payments');
 router.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+// crear una ruta para el pago de mercado pago
+router.post('/checkoutmp', initPaymentMp )
+// pago exitoso
+router.get('/success', (req, res) => {
+    console.log(req.query);
+    res.send('Pago exitoso');
+});
+// pago fallido
+router.get('/failure', (req, res) => {
+    console.log(req.query);
+    res.send('Pago failure');
+});
+// pago pendiente
+router.get('/pending', (req, res) => {
+    console.log(req.query);
+    res.send('Pago pending');
+});
 module.exports = router;
