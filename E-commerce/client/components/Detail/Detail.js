0ed20248b810d27b138d3ecbeeef90a485c6bfad
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-06-13 20:09:57 -0300

    a

--- E-commerce/client/components/Detail/Detail.js
+++ E-commerce/client/components/Detail/Detail.js
@@ -26,11 +26,12 @@ import { getAllCategories } from '../../redux/actions/categories';
 
     const product = useSelector((state)=> state.productReducer.details);
     console.log(product)
-    const sugestions = useSelector((state)=>state.productReducer.products).filter((ps)=>{
-      if(ps.category?.includes(product.category[0])){
-        return ps
-      }})
-    .slice(0,3)
+    // const sugestions = useSelector((state)=>state.productReducer.products).filter((ps)=>{
+    //   if(ps.category?.includes(product.category[0])){
+    //     return ps
+    //   }})
+    // .slice(0,3)
+    const sugestions = useSelector((state)=>state.productReducer.products).filter((ps)=>{return ps.category === product.category}).slice(0,5)
 
     useEffect(() => {
       if (!sugestions.length){
