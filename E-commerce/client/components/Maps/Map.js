0c5de6f46a743ab9048e6870fb6976657372f3ed
cristiannokk <83661346+cristiannokk@users.noreply.github.com>
2022-06-14 15:24:36 -0300

    feat(fix Maps from home)

--- E-commerce/client/components/Maps/Map.js
+++ E-commerce/client/components/Maps/Map.js
@@ -5,33 +5,33 @@ import {
   Flex,
 } from '@chakra-ui/react'
 import { useState } from 'react'
+import router from 'next/router'
 
 export default function Map() {
 
   const [map, setMap] = useState("Mendoza")
-
-  return (
-    <div>
-    <Flex>
-      <Button
-      onClick={() => setMap("Mendoza")}
-      >
-        Sucursal Mendoza, Argentina.
-      </Button>
-      <Button
-      onClick={() => setMap("Tucuman")}
-      >
-        Sucursal Tucuman, Argentina.
-      </Button>
-    </Flex>
-      <AspectRatio ratio={16 / 9}>
-        <iframe 
-          width="600" 
-          height="450" 
-          loading="lazy" 
-          src= {map === "Mendoza"? " https://www.google.com/maps/embed/v1/place?q=place_id:EjRQYXRyaWNpYXMgQXJnZW50aW5hcyA2NjUsIE1haXDDuiwgTWVuZG96YSwgQXJnZW50aW5hIjESLwoUChIJ_c3gfOoMfpYRFpsbxCr6sIQQmQUqFAoSCYFYdDPADH6WEdOuwchcCUaz&key=AIzaSyD7h-X8fVeczCSnntbmA0ORIIiVyEIXRSg": "https://www.google.com/maps/embed/v1/place?q=place_id:EklEci4gw4FuZ2VsIEdlcmFyZG8gUGlzYXJlbGxvLCBTYW4gTWlndWVsIGRlIFR1Y3Vtw6FuLCBUdWN1bcOhbiwgQXJnZW50aW5hIi4qLAoUChIJkfucVaVbIpQRVVZNJETBKXMSFAoSCQNpxdaSNyKUESdlxbWSW434&key=AIzaSyD7h-X8fVeczCSnntbmA0ORIIiVyEIXRSg"}>
-        </iframe>
-      </AspectRatio>
-    </div>
-  )
+  if(router.pathname === '/'){
+    return (
+      <div>
+      <Flex>
+        <Button
+        onClick={() => setMap("Mendoza")}
+        >
+          Sucursal Mendoza, Argentina.
+        </Button>
+        <Button
+        onClick={() => setMap("Tucuman")}
+        >
+          Sucursal Tucuman, Argentina.
+        </Button>
+      </Flex>
+        <AspectRatio ratio={16 / 9}>
+          <iframe 
+            width="600" 
+            height="450" 
+            loading="lazy" 
+            src= {map === "Mendoza"? " https://www.google.com/maps/embed/v1/place?q=place_id:EjRQYXRyaWNpYXMgQXJnZW50aW5hcyA2NjUsIE1haXDDuiwgTWVuZG96YSwgQXJnZW50aW5hIjESLwoUChIJ_c3gfOoMfpYRFpsbxCr6sIQQmQUqFAoSCYFYdDPADH6WEdOuwchcCUaz&key=AIzaSyD7h-X8fVeczCSnntbmA0ORIIiVyEIXRSg": "https://www.google.com/maps/embed/v1/place?q=place_id:EklEci4gw4FuZ2VsIEdlcmFyZG8gUGlzYXJlbGxvLCBTYW4gTWlndWVsIGRlIFR1Y3Vtw6FuLCBUdWN1bcOhbiwgQXJnZW50aW5hIi4qLAoUChIJkfucVaVbIpQRVVZNJETBKXMSFAoSCQNpxdaSNyKUESdlxbWSW434&key=AIzaSyD7h-X8fVeczCSnntbmA0ORIIiVyEIXRSg"}>
+          </iframe>
+        </AspectRatio>
+      </div>)} 
 }
