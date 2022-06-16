eef45fe35a216ab101671595c6a927f338134978
Matiaspp96 <matiaas.p@gmail.com>
2022-06-15 22:31:00 -0300

    fix:(getServerSideProps)

--- E-commerce/client/pages/userdata.js
+++ E-commerce/client/pages/userdata.js
@@ -3,48 +3,48 @@ import Data from "../components/User/UserData";
 import { BASEURL } from "../redux/actions/products";
 import { wrapper } from "../redux/store";
 
-function userDataGoogle({getUser}) {
+function userDataGoogle() {
 
 
   // console.log(getUser)
 
   return (
-    <Data getUser={getUser}/>
+    <Data />
   )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async (ctx) => {
-      console.log(ctx)
-      const urlUserData = `${BASEURL}/auth/data`;
-      const config = {
-            withCredentials: true,
-            headers: {
-              "Content-Type": "application/json",
-            },
-          };
-          console.log(config)
-          try{
-            const res = await fetch(urlUserData, {
-              method: 'GET',
-              withCredentials: true,
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            })
-            console.log(res)
-          // let getUser = await axios.get(urlUserData, config);
-          }catch(err){
-            console.log(err)
-          }
-          console.log(getUser.data)
-      return {
-        props: {
-          getUser: getUser.data
-        }
-      }
-    }
-);
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   (store) =>
+//     async (ctx) => {
+//       console.log(ctx)
+//       const urlUserData = `${BASEURL}/auth/data`;
+//       const config = {
+//             withCredentials: true,
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//           };
+//           console.log(config)
+//           try{
+//             const res = await fetch(urlUserData, {
+//               method: 'GET',
+//               withCredentials: true,
+//               headers: {
+//                 'Content-Type': 'application/json'
+//               }
+//             })
+//             console.log(res)
+//           // let getUser = await axios.get(urlUserData, config);
+//           }catch(err){
+//             console.log(err)
+//           }
+//           console.log(getUser.data)
+//       return {
+//         props: {
+//           getUser: getUser.data
+//         }
+//       }
+//     }
+// );
 
 export default userDataGoogle
