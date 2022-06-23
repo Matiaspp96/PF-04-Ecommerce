45e4ba2ac3676140cb87b1cb6b80a2a5f457498c
Matiaspp96 <matiaas.p@gmail.com>
2022-06-23 11:38:16 -0300

    LogoImg

--- E-commerce/client/components/Logo/Logo.js
+++ E-commerce/client/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import { Box, Text } from '@chakra-ui/react'
-import Logo from '../../public/Logo.svg'
+import logoImg from '../../public/Logo.svg'
 
 const Logo = (props) => {
   return (
     <Box pos='relative' minW='fit-content' minH='fit-content' overflow='hidden'>
-      <Image src={Logo} width='400' height='350' alt='Logo'/>
+      <Image src={logoImg} width='400' height='350' alt='Logo'/>
     </Box>
   )
 }
