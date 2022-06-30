189d98f2ec2c14ba7a312150f9347438bc9f829a
Matiaspp96 <matiaas.p@gmail.com>
2022-06-30 19:39:34 -0300

    Detail aling footer

--- E-commerce/client/pages/product/[id].js
+++ E-commerce/client/pages/product/[id].js
@@ -1,14 +1,15 @@
 import Navbar from '../../components/Navbar/Navbar'
 import Detail from '../../components/Detail/Detail'
 import Footer from '../../components/Footer/Footer'
+import { Stack } from '@chakra-ui/react'
 
 export default function DetailPage() {
     
     return (
-      <div>
+      <Stack alignItems='center'>
           <Navbar />
           <Detail />
           <Footer />
-      </div>
+      </Stack>
     )
   }
